80a506fca66ba954bf977a9e0dee2f145c06c1b0
sDiamond123 <sdiamond21@sidwell.edu>
2019-09-22 14:29:14 -0400

    Update DriveFTC.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
@@ -8,14 +8,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class DriveFTC extends OpMode {
 
     DcMotor leftFront;
+
     @Override
     public void init() {
-    leftFront=hardwareMap.dcMotor.get("leftFront");
-    leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
     public void loop() {
-    leftFront.setPower(-gamepad1.left_stick_y);
+        leftFront.setPower(-gamepad1.left_stick_y);
     }
 }
