e49c0874c651423319f4cde97eabf56144278758
sDiamond123 <sdiamond21@sidwell.edu>
2019-09-22 15:12:32 -0400

    adding trig Fun!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
@@ -21,6 +21,17 @@ public class DriveFTC extends OpMode {
 
     @Override
     public void loop() {
-        leftFront.setPower(-gamepad1.left_stick_y);
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        leftFront.setPower(v1);
+        rightFront.setPower(v2);
+        leftRear.setPower(v3)
+        rightRear.setPower(v4);
     }
 }
