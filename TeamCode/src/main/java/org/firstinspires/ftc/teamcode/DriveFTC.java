d51393cc0524204536e1026592be38dda696757e
sDiamond123 <sdiamond21@sidwell.edu>
2019-09-22 14:58:28 -0400

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
@@ -2,17 +2,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor
 
 @TeleOp
 public class DriveFTC extends OpMode {
 
-    DcMotor leftFront;
+    DcMotor leftFront, leftRear, rightFront,rightRear;
+
 
     @Override
     public void init() {
         leftFront = hardwareMap.dcMotor.get("leftFront");
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+        //`leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
