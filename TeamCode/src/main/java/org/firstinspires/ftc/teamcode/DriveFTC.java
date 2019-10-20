bc45ec96a3ac8e655133d2f06a3c778b7a19acf1
sDiamond123 <sdiamond21@sidwell.edu>
2019-10-20 15:46:52 -0400

    Update DriveFTC.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
@@ -10,6 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class DriveFTC extends OpMode {
     //boolean reverse=false;
     double speed=1;
+    double w1=0;
+    double w2=0;
     DcMotor leftFront, leftRear, rightFront, rightRear, linSlide1, linSlide2;
     double speedLeft=0;
     Servo wrist1, wrist2;
@@ -30,8 +32,6 @@ public class DriveFTC extends OpMode {
         //rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         //leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-    double w1=0;
-    double w2=0;
     @Override
     public void loop() {
 
@@ -45,45 +45,13 @@ public class DriveFTC extends OpMode {
         if (gamepad1.dpad_down) {
             speed = .2;
         }
-        if (!gamepad1.left_bumper || !gamepad1.right_bumper) {
             double x = gamepad1.left_stick_x * speed; // * (reverse ? -1 : 1);
             double y = -gamepad1.left_stick_y * speed; // * (reverse ? -1 : 1);
             double turn =-1* gamepad1.right_stick_y * speed;
-       /*double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y)    ;
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
-        final double v1 = r * Math.cos(robotAngle) + rightX;
-        final double v2 = r * Math.sin(robotAngle) - rightX;
-        final double v3 = r * Math.sin(robotAngle) + rightX;
-        final double v4 = r * Math.cos(robotAngle) - rightX;
-        */
             leftFront.setPower(x + y + turn);
             rightFront.setPower(-x + y - turn);
             leftRear.setPower(-x + y + turn);
             rightRear.setPower(x + y - turn);
-        }
-        else
-        {
-
-            if (gamepad1.left_bumper)
-            {
-                speedLeft+=.1;
-                speedRight-=.1;
-                speedRight=speedRight%1;
-                speedLeft=speedLeft%1;
-            }
-            else
-            {
-                speedLeft-=.1;
-                speedRight+=.1;
-                speedRight=speedRight%1;
-                speedLeft=speedLeft%1;
-            }
-            leftFront.setPower(speedLeft*speed);
-            leftRear.setPower(speedLeft*speed);
-            rightFront.setPower(speedRight*speed);
-            rightRear.setPower(speedRight*speed);
-        }
         //driving code end
 
         //arm code start
